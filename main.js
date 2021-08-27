54a0484f905990dad0fe0cc9b1b782cdbafccf64
Narah Kim <84653078+knarah@users.noreply.github.com>
2021-08-27 16:54:29 -0400

    JS: Navbar click event

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // 1.scrolling
 // 2. get Navbar's height
 
+//Make navbar appear when scrolled
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -10,4 +11,17 @@ document.addEventListener('scroll', () => {
     }else {
         navbar.classList.remove('navbar--dark');
     }
+});
+
+// Handling Scrolling - Navbar menu
+const navbarMenu = document.querySelector('.navbar__topmenu');
+
+navbarMenu.addEventListener('click', (event) => {
+    const link = event.target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+   scrollTo.scrollIntoView({behavior: "smooth"});
+    
 });
