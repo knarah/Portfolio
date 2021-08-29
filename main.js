b37ff19821f23e2535c56d7ad96452eaf6443c1a
Narah Kim <84653078+knarah@users.noreply.github.com>
2021-08-28 21:52:29 -0400

    JS: Topbutton

--- main.js
+++ main.js
@@ -36,3 +36,25 @@ function scrollIntoView(someElement) {
     scrollTo.scrollIntoView({behavior: "smooth"});
 }
 
+// Make 'Home' transparent when scrolling down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+console.log(homeHeight);
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY/homeHeight;
+});
+
+//Show Topbtn when scrolling down
+ const topbtn = document.querySelector('.topbtn');
+ document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight/2) {
+        topbtn.classList.add('visible');
+    } else {
+        topbtn.classList.remove('visible');
+    };
+ });
+
+ // Topbtn click -> Scroll up to Home
+ topbtn.addEventListener('click', () => {
+     scrollIntoView('#home');
+ });
